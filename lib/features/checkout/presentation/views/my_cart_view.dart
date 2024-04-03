103a05adf9e1e327103d6d9a3b3105617f0d2e3f
AhmedOsaama <ahmedosama49@gmail.com>
2024-04-03 05:20:06 +0200

    configured stripe

--- lib/features/checkout/presentation/views/my_cart_view.dart
+++ lib/features/checkout/presentation/views/my_cart_view.dart
@@ -1,24 +1,16 @@
-import 'package:checkout_app/core/utils/styles.dart';
-import 'package:checkout_app/features/checkout/presentation/views/widgets/my_cart_view_body.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
+import '../../../../core/widgets/cutom_app_bar.dart';
+import 'widgets/my_cart_view_body.dart';
+
 class MyCartView extends StatelessWidget {
-  const MyCartView({Key? key}) : super(key: key);
+  const MyCartView({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        leading: Center(child: SvgPicture.asset("arrow.svg")),
-        elevation: 0,
-        backgroundColor: Colors.transparent,
-        title: const Text(
-          "My Cart",
-          style: Styles.style24,
-        ),
-      ),
+      appBar: buildAppBar(title: 'My Cart'),
       body: const MyCartViewBody(),
     );
   }
